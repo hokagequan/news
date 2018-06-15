a954d6551104757cd1a212dcd1f1505fb020ba66
Quan <hokagequan@me.com>
2018-06-15 23:56:58 +0800

    创建表格

--- client/news/twodays/models.py
+++ client/news/twodays/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+
+    title = models.TextField()
+    url = models.TextField()
+    date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+		
